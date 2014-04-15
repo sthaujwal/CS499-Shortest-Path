a8344cb6c5b00dd9b6d0682a91c4e4e44ce839bc
sthaujwal <ujwal@uab.edu>
2014-04-15 01:39:37 -0500

    Update Distance.java
    This should compile !!

--- CS499/src/main/java/cs499blue/algorithms/Distance.java
+++ CS499/src/main/java/cs499blue/algorithms/Distance.java
@@ -8,15 +8,27 @@ import cs499blue.models.Vertex;
  * date: 4/1/14
  */
 
+package cs499blue.algorithms;
+
+import cs499blue.models.Edge;
+import cs499blue.models.Vertex;
+
+/**
+ * author: vwilson
+ * date: 4/1/14
+ */
+
 public class Distance {
 
     //returns the distance from a to b
     public static Double distanceFrom(Vertex a, Vertex b) {
-        throw new UnsupportedOperationException("Not Yet Implemented");
+        
+        return Math.sqrt(Math.pow((a.getX()-b.getX()),2) + Math.pow((a.getY() - b.getY()),2));
     }
+    
 
     //returns the length of e
     public static Double lengthOf(Edge e) {
-        throw new UnsupportedOperationException("Not Yet Implemented");
+        return distanceFrom(e.getA(), e.getB());
     }
 }
